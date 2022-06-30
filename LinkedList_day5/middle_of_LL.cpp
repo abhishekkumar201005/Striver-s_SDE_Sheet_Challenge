d39fb411242cef25f76e81770c599a507fa8838c
AbhishekRana <abhishekrana201005@gmail.com>
2022-06-30 22:24:51 +0530

    1st day of LL

---
+++ LinkedList_day5/middle_of_LL.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+
+//-------CODE    ONLY---------------------------------
+    // ListNode *middleNode(ListNode *head)
+    // {
+    //     ListNode *slow = head;
+    //     ListNode *fast = head;
+    //     while (fast != NULL && fast->next != NULL)
+    //     {
+    //         slow = slow->next;
+    //         fast = fast->next->next;
+    //     }
+    //     return slow;
+    // }
